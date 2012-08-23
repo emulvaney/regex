3a1f63ff218fb67163ebe0e1be6cc4aa39c8b6fe
Eric Mulvaney <eric.mulvaney@gmail.com>
2012-08-23 16:17:38 -0400

    Added GPLv2 and copyleft notices throughout.

--- grep.c
+++ grep.c
@@ -1,6 +1,18 @@
-/* My grep Implementation
+/* A Regular Expression Library - Example Grep Implementation
  * Copyright (c) 2012 Eric Mulvaney
- * TODO: Add GPL3 bit here
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <ctype.h>
